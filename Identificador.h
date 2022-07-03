3f86998be6f4032d035bd31e7e087bab52a57343
LaraTana <105310350+LaraTana@users.noreply.github.com>
2022-07-03 18:42:19 -0300

    Mudando corpo do identificador

--- Identificador.h
+++ Identificador.h
@@ -3,16 +3,18 @@
 #include <time.h>
 
 
-
 int id(){
-    int numero;
-    int min = 0;
-    int max = 10000;
-
+    int id[1000];
+    int soma =0;
     srand(time(NULL));
 
-    numero = (rand() % (min + max * 3))+ max;
-      
+    for (int i =0; i<=5; i++){
+        id[i]= rand()%10;
+        soma = id[i]+ soma*10;
+        
+           
+    }
+    printf("%d\n", soma);
+    return soma;
 
-      return numero;
-};
+}
