04b259b5d737925180a98b457503fef157264ded
LaraTana <105310350+LaraTana@users.noreply.github.com>
2022-07-05 17:20:16 -0300

    Identificador completo

--- Identificador.h
+++ Identificador.h
@@ -3,18 +3,21 @@
 #include <time.h>
 
 
-int id(){
-    int id[1000];
-    int soma =0;
-    srand(time(NULL));
 
-    for (int i =0; i<=5; i++){
-        id[i]= rand()%10;
-        soma = id[i]+ soma*10;
-        
-           
-    }
-    printf("%d\n", soma);
-    return soma;
+identificador(){
+  int id; 
+  int sort_id =0 ;
+  srand(time(NULL));
+
+  sort_id = rand()%9;
+  if (sort_id % 2 == 0){
+    id = 1000 + rand()%5000;
+    printf("%d\n", id);
+  }else {
+    id = 5000 + rand()%5000;
+    printf("%d\n", id);
+  }
+  
+  return id;
 
 }
