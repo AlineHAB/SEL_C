9a9d4491278c378c5c3ca95f955125a6d8ceff64
Aline Henrique Alves Barros <89356097+AlineHAB@users.noreply.github.com>
2022-07-11 16:16:02 -0300

    Update Identificador.h

--- Identificador.h
+++ Identificador.h
@@ -5,19 +5,19 @@
 
 
 int identificador(){
-  int id; 
-  int sort_id =0 ;
-  srand(time(NULL));
+    int id; 
+    int sort_id =0 ;
+    srand(time(NULL));
 
-  sort_id = rand()%9;
-  if (sort_id % 2 == 0){
-    id = 1000 + rand()%5000;
-    printf("%d\n", id);
-  }else {
-    id = 5000 + rand()%5000;
-    printf("%d\n", id);
-  }
-  
-  return id;
+    sort_id = rand()%9;
+    if (sort_id % 2 == 0){
+      id = 1000 + rand()%5000;
+      printf("%d\n", id);
+    }else {
+      id = 5000 + rand()%5000;
+      printf("%d\n", id);
+    }
+
+   return id;
 
 }
